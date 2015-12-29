a9e8dcf0b2f71036753ea30930949183e378363f
Sarel van der Walt <sarel.w@gmail.com>
2015-12-29 15:04:24 +0200

    Line-formatting config option and enable flag for default handle-stratagy

--- DependencyInjection/AfrihostBaseCommandExtension.php
+++ DependencyInjection/AfrihostBaseCommandExtension.php
@@ -23,5 +23,16 @@ class AfrihostBaseCommandExtension extends Extension
 
         $container->setParameter('afrihost_base_command.logger.handler_strategies.default.file_extention', $config['logger']['handler_strategies']['default']['file_extention']);
         $container->setParameter('afrihost_base_command.locking.enabled', $config['locking']['enabled']);
+        $container->setParameter('afrihost_base_command.logger.line_formatting', $config['logger']['line_formatting']);
+
+        foreach ($config['logger']['handler_strategies'] as $strategyName => $strategyConfig) {
+            $container->setParameter('afrihost_base_command.logger.handler_strategies.' . $strategyName . '.enabled', true); // By default all strategies are enabled at this time
+            foreach ($strategyConfig as $strategyConfigKey => $strategyConfigDetail) {
+                $container->setParameter(
+                    'afrihost_base_command.logger.handler_strategies.' . $strategyName . '.' . $strategyConfigKey,
+                    $config['logger']['handler_strategies'][$strategyName][$strategyConfigKey]
+                );
+            }
+        }
     }
 }
