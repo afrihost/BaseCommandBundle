f0875b5d85fb4e3cafbb9dda1103c3d51345a011
Brad Mostert <mostertb@users.noreply.github.com>
2016-01-09 14:45:05 +0200

    Add test commands to fixtures

---
+++ Tests/Fixtures/LoggingCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Afrihost\BaseCommandBundle\Tests\Fixtures;
+
+
+use Afrihost\BaseCommandBundle\Command\BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LoggingCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('test:logging')
+            ->setDescription('This command makes one log entry for each logging level');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getLogger()->debug('DEBUG');
+        $this->getLogger()->info('INFO');
+        $this->getLogger()->notice('NOTICE');
+        $this->getLogger()->warn('WARNING');
+        $this->getLogger()->error('ERROR');
+        $this->getLogger()->crit('CRITICAL');
+        $this->getLogger()->alert('ALERT');
+        $this->getLogger()->emerg('EMERGENCY');
+    }
+}
