79765f84199afb3a4453e869a054b1fe669e3b74
Brad Mostert <mostertb@users.noreply.github.com>
2016-02-22 22:50:59 +0200

    Add test coverage for log LineFormat

--- Tests/Helper/Logging/LoggingEnhancementTest.php
+++ Tests/Helper/Logging/LoggingEnhancementTest.php
@@ -7,6 +7,7 @@ use Afrihost\BaseCommandBundle\Tests\Fixtures\ConfigDuringExecuteCommand;
 use Afrihost\BaseCommandBundle\Tests\Fixtures\EncapsulationViolator;
 use Afrihost\BaseCommandBundle\Tests\Fixtures\HelloWorldCommand;
 use Afrihost\BaseCommandBundle\Tests\Fixtures\LoggingCommand;
+use Monolog\Logger;
 
 class LoggingEnhancementTest extends AbstractContainerTest
 {
@@ -47,25 +48,99 @@ class LoggingEnhancementTest extends AbstractContainerTest
 
     public function testDefaultLineFormatter()
     {
+        $logfileName = 'defaultFormatterTest.log';
+        $this->cleanUpLogFile($logfileName);
+
         $command = $this->registerCommand(new LoggingCommand());
+        $command->setLogFilename($logfileName);
         $commandTester = $this->executeCommand($command);
 
+        // Test default console format
         $this->assertRegExp(
             '/20\d\d-[01]\d-[0-3]\d [0-2]\d:[0-5]\d:[0-5]\d \[WARNING\]: WARNING/',
             $commandTester->getDisplay(),
-            'Expected log entry format not found'
+            'Expected default log entry format for Console Log not found'
         );
 
-        // TODO test default file line format
+        // Test default logfile format
+        $this->assertRegExp(
+            '/20\d\d-[01]\d-[0-3]\d [0-2]\d:[0-5]\d:[0-5]\d \[WARNING\]: WARNING/',
+            $this->getLogfileContents($logfileName),
+            'Expected default log entry format for Console Log not found'
+        );
+
+        $this->cleanUpLogFile($logfileName);
     }
 
-    // TODO Test setting custom console log line format
+    public function testCustomLogLineFormats()
+    {
+        $logfileName = 'customFormatterTest.log';
+        $this->cleanUpLogFile($logfileName);
 
-    // TODO Test setting custom file log line format
+        $command = $this->registerCommand(new LoggingCommand());
+        $command->setLogFilename($logfileName);
+        $command->setConsoleLogLineFormat('Writing message to console: %message%');
+        $command->setFileLogLineFormat('Writing message to log file: %message%');
+        $commandTester = $this->executeCommand($command);
 
-    // TODO Test using monolog default line format for console log
+        // Test  console format
+        $this->assertRegExp(
+            '/Writing message to console: The quick brown fox jumps over the lazy dog/',
+            $commandTester->getDisplay(),
+            'Expected custom log entry format for Console Log not found'
+        );
 
-    // TODO Test using monolog default line format for file log
+        // Test default logfile format
+        $this->assertRegExp(
+            '/Writing message to log file: The quick brown fox jumps over the lazy dog/',
+            $this->getLogfileContents($logfileName),
+            'Expected default log entry format for Console Log not found'
+        );
+
+        $this->cleanUpLogFile($logfileName);
+    }
+
+    public function testProvidingNullLineFormatToGetMonologDefault()
+    {
+        $logfileName = 'nullFormatterTest.log';
+        $this->cleanUpLogFile($logfileName);
+
+        $command = $this->registerCommand(new LoggingCommand());
+        $command->setLogFilename($logfileName);
+        $command->setConsoleLogLineFormat(null);
+        $command->setFileLogLineFormat(null);
+        $commandTester = $this->executeCommand($command);
+
+        // Generate what the default format looks like
+        $lineFormatter = new \Monolog\Formatter\LineFormatter(null);
+        $record = array(
+            'message' => 'The quick brown fox jumps over the lazy dog',
+            'context' => array(),
+            'level' => Logger::EMERGENCY,
+            'level_name' => Logger::getLevelName(Logger::EMERGENCY),
+            'channel' => $command->getLogger()->getName(),
+            'datetime' => new \DateTime('1970-01-01 00:00:00'),
+            'extra' => array(),
+        );
+        $exampleLine = $lineFormatter->format($record);
+        $exampleLine = trim(str_replace('[1970-01-01 00:00:00]', '', $exampleLine)); // strip out date as this wont match
+
+        // Test  console format
+        $this->assertRegExp(
+            '/'.$exampleLine.'/',
+            $commandTester->getDisplay(),
+            'Console log line format does not seem to match the Monolog default'
+        );
+
+        // Test default logfile format
+        $this->assertRegExp(
+            '/'.$exampleLine.'/',
+            $this->getLogfileContents($logfileName),
+            'File log line format does not seem to match the Monolog default'
+        );
+
+        $this->cleanUpLogFile($logfileName);
+    }
 
     // TODO Test Set Default File Extension
 
