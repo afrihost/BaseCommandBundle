b3848217ae259af859d070e5093e86b0052cd9a8
Roscoe van Wyk <roscoew@afrihost.com>
2017-05-29 16:01:29 +0200

    Update copyright to 2017 and new business entity

--- Command/BaseCommand.php
+++ Command/BaseCommand.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2015 Afrihost Internet Services (Pty) Ltd
+ * Copyright (c) 2017 Afrihost SP (Pty) Ltd
  * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
  */
 
